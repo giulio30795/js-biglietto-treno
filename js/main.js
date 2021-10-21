46f6bf67aaaacf043be979c75f215e56d65dac6b
giulio30795 <giulioincarbone95@gmail.com>
2021-10-21 16:14:28 +0200

    final check

--- js/main.js
+++ js/main.js
@@ -11,15 +11,16 @@ let prezzo = (km*0.21).toFixed(2);
 const over65 = (prezzo*40/100)
 const minorenni = (prezzo*20/100)
 
-// Alert
-if (isNaN(età) || isNaN(km)) {
-    alert('Dati inseriti non validi, riprovare.');
-}
 // Calcolo prezzo scontato
 if (età < 18) {
     prezzo -= minorenni.toFixed(2);
 } else if (età > 65) {
     prezzo -= over65.toFixed(2);
 }
+
 // Output risultato
-document.getElementById('prezzo').innerHTML =`il prezzo del biglietto è: ${prezzo}`;
+if (isNaN(età) || isNaN(km)) {
+    alert('Dati inseriti non validi, riprovare.');
+} else {
+    document.getElementById('prezzo').innerHTML =`il prezzo del biglietto è: ${prezzo}`;
+}
