f5fa692635dcdde16e11db2116a95558a9357e5f
giulio30795 <giulioincarbone95@gmail.com>
2021-10-21 15:08:16 +0200

    check

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ const km = parseFloat (prompt('Quanti km vuoi fare?') );
 // Calcolo prezzo standard 0,21*KM
 let prezzo = (km*0.21).toFixed(2);
 // Calcolo prezzo scontato minorenni -20%
+
+const over65 = (prezzo*40/100)
+const minorenni = (prezzo*20/100)
+
 if (età < 18) {
-    prezzo = (prezzo-(prezzo*20/100)).toFixed(2);
+    prezzo = (prezzo-minorenni).toFixed(2);
 }
 // Calcolo prezzo scontato over 65 -40%
 if (età > 65) {
-    prezzo = (prezzo-(prezzo*40/100)).toFixed(2);
+    prezzo = (prezzo-over65).toFixed(2);
 }
 // Output risultato
 document.getElementById('prezzo').innerHTML =`il prezzo del biglietto è: ${prezzo}`;
